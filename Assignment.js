5d85f76645d2dcaf21a406cb74144e6d58f4747c
Rajajr <rajjr011@gmail.com>
2022-11-17 18:48:32 +0700

    UC-2 Read a Number and Display the week day

--- Assignment.js
+++ Assignment.js
@@ -1,45 +1,46 @@
-//UC-1 Read a single digit Number and Write The Number In Words Using Case
+//UC-2 read a number and Display the week day
 
-    var readlineSync=require('readline-sync');
-    var number=readlineSync.question('Enter the number: ');
+var readlineSync=require('readline-sync');
+var number=readlineSync.question('Enter the number: ');
+
+switch(number)
+{
     
-    switch(number)
-    {
-        case "0":
-           console.log('zero')
-        break;
-        case "1":
-           console.log('unit')
-            
-        break;
-        case "10":
-            console.log('Ten')
-        break;
-        case "100":
-            console.log('Hundread')
-        break
-        case '1000':
-          console.log('Thousand')
-        break;
-        case '10000':
-          console.log("Ten Thousand")
-        break;
-        case '100000':
-            console.log("Lakhs")
-        break;
-        case '1000000':
-            console.log("Ten Lakhs")
-        break;
-        case '10000000':
-            console.log("Crores")
-        break;
-    
-        default:
-            console.log('Enter the Number correct');
-        break;
+    case 1:
+       console.log('Sunday')
         
+    break;
+
+    case 2:
+        console.log('Monday')
+    break;
+
+    case 3:
+        console.log('Tuesday')
+    break;
+
+    case 4:
+      console.log('Wednesday')
+    break;
+
+    case 5:
+      console.log("Thursday")
+    break;
+
+    case 6:
+        console.log("Friday")
+    break;
     
-    }
+    case 7:
+        console.log("Saturday")
+    break;
+    
+    default:
+        console.log('Enter the Number correct');
+    break;
+    
+
+}
 
 
    
