707c7a4c7f31e897eaf49ef04d1f64c653781b2e
Rajajr <rajjr011@gmail.com>
2022-11-19 06:20:09 +0700

    UC-4 Write a program That User Inputs and Does Unit Coversion Of Different Length Units

--- Assignment.js
+++ Assignment.js
@@ -1,53 +1,41 @@
 //UC-3 Read a single digit number and write the number in word using case
 
-console.log('Enter the Number From 1 to 10');
+console.log('Welcom to The Programe');
 var readlineSync=require('readline-sync');
-var number=readlineSync.question('Enter the number: ');
+var option=readlineSync.question("1:Feet to Meter\t2:Feet to Inch\t3:Inch to Feet\t4:Meter to Feet");
+console.log("-------------------------------------");
+console.log("Scelet the Option : ");
 
-switch(number)
+
+switch(option)
 {
     
-    case 1:
-       console.log('ONE')
+    case "1":
+
+       let num1=readlineSync.question("Enter the Number :");
+       meter=num1*0.305;
+       console.log("Feet To Meter Conversion is : "+meter+"m");
         
     break;
 
-    case 2:
-        console.log('TWO')
+    case "2":
+       let num2=readlineSync.question("Enter the Number :");
+       let result1=num2*12;
+       console.log("Feet to Inch is : "+result+"inch");
     break;
 
-    case 3:
-        console.log('THREE')
+    case "3":
+        let num3=readlineSync.question("Enter the Number :");
+        feet =num3*0.35;
+        console.log("Inch to Feet conversion is : "+feet+"Feet");
     break;
 
-    case 4:
-      console.log('FOUR')
+    case "4":
+        let num4=readlineSync.question("Enter the Number :");
+        mfeet=num4*3.28;
+        console.log("Meter to Feet conversion is : "+mfeet+"Feet");
     break;
 
-    case 5:
-      console.log("FIVE")
-    break;
-
-    case 6:
-        console.log("SIX")
-    break;
-    
-    case 7:
-        console.log("SEVEN")
-    break;
-
-    case 8:
-        console.log("EIGHT")
-    break;
-
-    case 9:
-        console.log("NINE")
-    break;
-
-    case 10:
-        console.log("TEN")
-    break;
-    
     default:
         console.log('Number is incorrect');
     break;
